ea75d96493c805d6cb1443cd9358a02e26ca79de
Pavan Kumar Kavvuri <jy19201@bristol.ac.uk>
2020-03-31 12:37:02 +0100

    Reach Goal, PathPlanning .... inprogress

--- src/turtlebotMotion/motionFile.cpp
+++ src/turtlebotMotion/motionFile.cpp
@@ -1,37 +1,62 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
-void moveForward(double speed, double distance, bool isForward);
+#include "turtlesim/Pose.h"
+#include "sstream"
+#include <iostream>
 
+using namespace std;
+
+void moveForward(double speed, double distance, bool isForward);
+double deg2Rad(double deg);
+void rotate2Angle(double speed, double des);
+double rad2Deg(double rad);
+//void print(sstream ss, double t);
 //double t0 = 0;
 double distance = 4;
 double curr_dist = 0;
 double speed = 1;
-double pi = 22/7;
+const double pi = 3.14;
+
+
 ros::Publisher pub;
 ros::Subscriber sub;
+turtlesim::Pose currentPose;
+
+void poseCallback(const turtlesim::Pose::ConstPtr &pose);
+
+
 int main(int argv, char **argc){
 
     ros::init(argv, argc, "motionBot");
     ros::NodeHandle n;
-    ros::Rate loop_rate(100);
-    double t0 = ros::Time::now().toSec();
-
-    pub = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 10);
-    sub = n.subscribe(/turtle1/pose, 10, poseCallback);
-
-    geometry_msgs::Twist velocity;
+    double angularSpeed = 0;
+    double angle_des = 0;
+   
     cout<<"Angular Speed : ";
     cin>> angularSpeed;
-    cout<<"Desired Orientation";
+    cout<<"Desired Orientation : ";
     cin>>angle_des;
 
-    rotate2Angle(deg2Rad(angularSpeed), deg2Rad(angle_des));
+    //rotate2Angle(deg2Rad(angularSpeed), deg2Rad(angle_des));
+    deg2Rad(angularSpeed);   deg2Rad(angle_des);
 
-   
+    pub = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
+    sub = n.subscribe("/turtle1/pose", 10, poseCallback);
+    rotate2Angle( angularSpeed,  angle_des);
+
+  
+    return 0;
+    
 }
 
 void rotate2Angle(double speed, double des){
-    double diffAngle = des - currentPose;
+     geometry_msgs::Twist velocity;
+     
+    double diffAngle = des - currentPose.theta;
+   // cout<< "currentPose is : " << currentPose << endl;
+    cout<< "diff angle  is : " << diffAngle << endl;
+   // print("Hello", 56);
+    bool clockwise = ((diffAngle > 0) ? false : true);
 
         velocity.linear.x = 0;
         velocity.linear.y = 0;
@@ -39,26 +64,61 @@ void rotate2Angle(double speed, double des){
         
         velocity.angular.x = 0;
         velocity.angular.y = 0;
-        velocity.angular.z = speed;
 
-     while(curr_dist < distance){
-       
-        curr_dist = abs(speed) * ( ros::Time::now().toSec() - t0 );
+        if(clockwise){
+            velocity.angular.z = -speed;
+        }else{
+            velocity.angular.z = speed;
+        }
+   // cout<< "Velocity is : " << velocity<<endl; 
+    double angleCovered = 0.0;
+    double t0 = ros::Time::now().toSec();
+    double count = 0;
+    ros::Rate loop_rate(10);
+    try{
+
+        do{
+            pub.publish(velocity);
+            //cout<< "Count is : "<< count++ << endl;
+            double time = ros::Time::now().toSec() - t0 ;
+            //curr_dist = abs(speed) * ( ros::Time::now().toSec() - t0 );
+            //angleCovered = speed * time;
+            cout<< "timeCovered is : " << speed * time << endl;
+            ros::spinOnce();
+            loop_rate.sleep();
+            //ros::Duration(2).sleep();
+        }while(angleCovered < 1.57);
+        velocity.angular.z = 0;
         pub.publish(velocity);
-        ros::spinOnce();
-        loop_rate.sleep();
+
+    }catch(ros::Exception &e){
+        ROS_ERROR("ROs error occured %s", e.what());
     }
-     velocity.linear.x = 0;
-     pub.publish(velocity);
+     
+
+     
     
 }
 
-void poseCallback(const turtlesim::pose::Constptr &pose){
-   currentPose  = pose.theta;
-   cout<<"currentPose is : "<< currentPose;
+void poseCallback(const turtlesim::Pose::ConstPtr &pose){
+    
+   currentPose.theta  = pose->theta;
+   cout<< "pose is : "<< currentPose.theta << endl;
+   
 }
 
+//void print(sstream ss, double t){
+ //    cout<< ss << t << endl;
+//}
+
 double deg2Rad(double deg){
+    cout<<"Angular values in Radians: ";
+    cout<< deg * pi/180;
+   
     return deg * pi / 180;
+}
+
+double rad2Deg(double rad){
+    return rad * 180 / pi;
 }
 
